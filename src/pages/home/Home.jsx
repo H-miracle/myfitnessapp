04024b340b87f8af9f34eed917b89bcded62601e
H-miracle <deboimiracle@gmail.com>
2022-09-06 03:17:32 +0000

    testimonials section added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import MainHeader from '../../components/MainHeader'
 import Programs from '../../components/Programs'
 import Values from '../../components/Values'
 import FAQs from '../../components/FAQs'
+import Testimonials from '../../components/Testimonials'
 
 
 const Home = () => {
@@ -12,6 +13,7 @@ const Home = () => {
       <Programs/>
       <Values />
       <FAQs />
+      <Testimonials/>
     </>
   )
 }
